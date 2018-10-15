3f01e620d3df40e921b7e9c47964d929831c15b1
first <first@first-405>
2018-10-15 14:05:49 +0500

    Циклический ввод данных в таблицу Department

--- src/main/java/org/ivc/transportation/TransportationApplication.java
+++ src/main/java/org/ivc/transportation/TransportationApplication.java
@@ -11,23 +11,29 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransportationApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(TransportationApplication.class, args);
     }
-    
+
     @Bean
     ApplicationRunner init(DepartmentRepository dpartmentRepository) {
         return (ApplicationArguments args) -> {
-           Stream.of("ЦИ-1", "ЦИ-2", "ЦИ-4", "ЦИ-ТК", "ЦИ-7",
-                    "КЭССТ", "КЭЗС", "КТМО", "СЛУЖБА БЕЗОПАСНОСТИ").forEach(name -> {
-            Department dep = new Department();
-            dep.setName(name);
-        //    dep.setAddress("площадка №2");
-            dpartmentRepository.save(dep);
-                    });
+
+            String[] name = {"ЦИ-1", "ЦИ-2", "ЦИ-4", "ЦИ-ТК", "ЦИ-7",
+                "КЭССТ", "КЭЗС", "КАТО", "СЛУЖБА БЕЗОПАСНОСТИ"};
+            String[] addres = {"Площадка №1", "Площадка №2", "Титова 7", "Нет адреса", "Площадка №254",
+                "Советской армии 5", "В степи по дороге", "Советской армии 7", "Везде"};
+            
+            for (int i = 0; i < name.length; i++) {
+                Department dep = new Department();
+                dep.setName(name[i]);
+                dep.setAddres(addres[i]);
+                dpartmentRepository.save(dep);
+            }
+
             dpartmentRepository.findAll().forEach(System.out::println);
         };
     }
-    
+
 }
