59594ea43c26f2ef28b0632c6a88be51444f0a1c
first <first@first-405>
2018-10-23 20:49:55 +0600

    Внесены следующие изменения: 	- Добавлен PlanService; 	- Изменен TransportDepService; 	- Удален VechicleService; 	- Удален DriverService; 	- Удален VechicleController; 	- Удален DriverController; 	- Изменен TransportDepController.

--- src/main/java/org/ivc/transportation/controllers/TransportDepController.java
+++ src/main/java/org/ivc/transportation/controllers/TransportDepController.java
@@ -14,9 +14,7 @@ import java.util.Optional;
 import org.ivc.transportation.entities.Driver;
 import org.ivc.transportation.entities.TransportDep;
 import org.ivc.transportation.entities.Vechicle;
-import org.ivc.transportation.services.DriverService;
 import org.ivc.transportation.services.TransportDepService;
-import org.ivc.transportation.services.VechicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,51 +27,98 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TransportDepController {
 
-    private static final String url = "/transportdeps";
+    private static final String URL_T = "/transportdeps";
+    private static final String URL_D = "/drivers";
+    private static final String URL_V = "/vechicles";
 
     @Autowired
-    private TransportDepService localServ;
+    private TransportDepService tdServ;
 
-    @Autowired
-    private DriverService drvServ;
-
-    @Autowired
-    private VechicleService vechServ;
-
-    @GetMapping(url)
+    @GetMapping(URL_T)
     public Collection<TransportDep> getTransportDeps() {
-        return localServ.getTransportDeps();
+        return tdServ.getTransportDeps();
     }
 
-    @GetMapping(url + "/{id}")
+    @GetMapping(URL_T + "/{id}")
     public TransportDep getTransportDep(@PathVariable Long id) {
-        Optional<TransportDep> dep = localServ.getTransportDepById(id);
+        Optional<TransportDep> dep = tdServ.getTransportDepById(id);
         return dep.get();
     }
 
-    @DeleteMapping(url + "/{id}")
+    @DeleteMapping(URL_T + "/{id}")
     public void delTransportDep(@PathVariable Long id) {
-        localServ.removeTransportDep(id);
+        tdServ.removeTransportDep(id);
     }
 
-    @PostMapping(url)
+    @PostMapping(URL_T)
     public void addTransportDep(@RequestBody TransportDep department) {
-        localServ.addTransportDep(department);
+        tdServ.addTransportDep(department);
     }
 
-    @PutMapping(url + "/{id}")
+    @PutMapping(URL_T + "/{id}")
     public void updateTransportDep(@RequestBody TransportDep dep, @PathVariable Long id) {
-        localServ.updateTransportDep(dep, id);
+        tdServ.updateTransportDep(dep, id);
     }
 
-    @GetMapping(url + "/{id}/drivers")
+    @GetMapping(URL_T + "/{id}/drivers")
     public Collection<Driver> getDrivers(@PathVariable Long id) {
-        return drvServ.getDriversByTransportDepId(id);
+        return tdServ.getDriversByTransportDepId(id);
     }
 
-    @GetMapping(url + "/{id}/vechicles")
+    @GetMapping(URL_T + "/{id}/vechicles")
     public Collection<Vechicle> getVechicles(@PathVariable Long id) {
-        return vechServ.getVechiclesByTransportDepId(id);
+        return tdServ.getVechiclesByTransportDepId(id);
     }
 
+    @GetMapping(URL_D)
+    public Collection<Driver> getDrivers() {
+        return tdServ.getDrivers();
+    }
+
+    @GetMapping(URL_D + "/{id}")
+    public Driver getDriver(@PathVariable Long id) {
+        Optional<Driver> dep = tdServ.getDriverById(id);
+        return dep.get();
+    }
+
+    @DeleteMapping(URL_D + "/{id}")
+    public void delDriver(@PathVariable Long id) {
+        tdServ.removeDriver(id);
+    }
+
+    @PostMapping(URL_D)
+    public void addDriver(@RequestBody Driver department) {
+        tdServ.addDriver(department);
+    }
+
+    @PutMapping(URL_D + "/{id}")
+    public void updateDriver(@RequestBody Driver dep, @PathVariable Long id) {
+        tdServ.updateDriver(dep, id);
+    }
+
+    @GetMapping(URL_V)
+    public Collection<Vechicle> getVechicles() {
+        return tdServ.getVechicles();
+    }
+
+    @GetMapping(URL_V + "/{id}")
+    public Vechicle getVechicle(@PathVariable Long id) {
+        Optional<Vechicle> dep = tdServ.getVechicleById(id);
+        return dep.get();
+    }
+
+    @DeleteMapping(URL_V + "/{id}")
+    public void delVechicle(@PathVariable Long id) {
+        tdServ.removeVechicle(id);
+    }
+
+    @PostMapping(URL_V)
+    public void addVechicle(@RequestBody Vechicle department) {
+        tdServ.addVechicle(department);
+    }
+
+    @PutMapping(URL_V + "/{id}")
+    public void updateVechicle(@RequestBody Vechicle dep, @PathVariable Long id) {
+        tdServ.updateVechicle(dep, id);
+    }
 }
--- src/main/java/org/ivc/transportation/services/TransportDepServiceImpl.java
+++ src/main/java/org/ivc/transportation/services/TransportDepServiceImpl.java
@@ -7,8 +7,12 @@ package org.ivc.transportation.services;
 
 import java.util.Collection;
 import java.util.Optional;
+import org.ivc.transportation.entities.Driver;
 import org.ivc.transportation.entities.TransportDep;
+import org.ivc.transportation.entities.Vechicle;
+import org.ivc.transportation.repositories.DriverRepository;
 import org.ivc.transportation.repositories.TransportDepRepository;
+import org.ivc.transportation.repositories.VechicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,38 +24,128 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class TransportDepServiceImpl implements TransportDepService {
-    
+
     @Autowired
-    private TransportDepRepository localRep;
-    
+    private TransportDepRepository tdRep;
+    @Autowired
+    private DriverRepository drRep;
+    @Autowired
+    private VechicleRepository vclRep;
+
     @Override
     @Transactional
     public void addTransportDep(TransportDep d) {
-        this.localRep.save(d);
+        this.tdRep.save(d);
     }
-    
+
     @Override
     @Transactional
     public void updateTransportDep(TransportDep d, Long id) {
         d.setId(id);
-        localRep.save(d);
-    }       
-    
+        tdRep.save(d);
+    }
+
     @Override
     @Transactional
     public void removeTransportDep(Long id) {
-        localRep.deleteById(id);
+        tdRep.deleteById(id);
     }
 
     @Override
     @Transactional
     public Collection<TransportDep> getTransportDeps() {
-        return localRep.findAll();
+        return tdRep.findAll();
     }
-    
+
     @Override
     @Transactional
     public Optional<TransportDep> getTransportDepById(Long id) {
-        return localRep.findById(id);
+        return tdRep.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void addDriver(Driver d) {
+        this.drRep.save(d);
+    }
+
+    @Override
+    @Transactional
+    public void updateDriver(Driver d, Long id) {
+        d.setId(id);
+        drRep.save(d);
+    }
+
+    @Override
+    @Transactional
+    public void removeDriver(Long id) {
+        drRep.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public Optional getDriverById(Long id) {
+        return drRep.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Collection<Driver> getDrivers() {
+        return drRep.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Collection<Driver> findDriversByVacant(Boolean d) {
+        return drRep.findByVacant(d);
+    }
+
+    @Override
+    @Transactional
+    public Collection<Driver> getDriversByTransportDepId(Long id) {
+        return drRep.findByTransportDepId(id);
+    }
+
+    @Override
+    @Transactional
+    public void addVechicle(Vechicle d) {
+        this.vclRep.save(d);
+    }
+
+    @Override
+    @Transactional
+    public void updateVechicle(Vechicle d, Long id) {
+        d.setId(id);
+        vclRep.save(d);
+    }
+
+    @Override
+    @Transactional
+    public void removeVechicle(Long id) {
+        vclRep.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public Collection<Vechicle> getVechicles() {
+        return vclRep.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Collection<Vechicle> findVechiclesByVacant(Boolean d) {
+        return vclRep.findByVacant(d);
+    }
+
+    @Override
+    @Transactional
+    public Collection<Vechicle> getVechiclesByTransportDepId(Long id) {
+        return vclRep.findByTransportDepId(id);
+    }
+
+    @Override
+    @Transactional
+    public Optional getVechicleById(Long id) {
+        return vclRep.findById(id);
     }
 }
