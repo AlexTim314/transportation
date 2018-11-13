3636284adcef35d9697e7edd1da5bf2cbf888694
AlexTim <alextim@ivc.org>
2018-11-13 11:20:29 +0600

    After vacation. Need to pull.

--- src/main/java/org/ivc/transportation/controllers/ControllersExceptionHandler.java
+++ src/main/java/org/ivc/transportation/controllers/ControllersExceptionHandler.java
@@ -5,11 +5,8 @@
  */
 package org.ivc.transportation.controllers;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.ivc.transportation.exceptions.NonExistingDepartmentException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
