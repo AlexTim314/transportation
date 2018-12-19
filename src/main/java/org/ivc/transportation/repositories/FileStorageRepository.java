ec962bff4714b39fc2ba1f023d5fb93fef4c30d1
first <first@first-405>
2018-12-19 11:04:11 +0600

    Перезаливаю проект

---
+++ src/main/java/org/ivc/transportation/repositories/FileStorageRepository.java
@@ -0,0 +1,16 @@
+package org.ivc.transportation.repositories;
+
+import java.util.List;
+import org.ivc.transportation.entities.FileStorage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author first
+ */
+@Repository
+public interface FileStorageRepository extends JpaRepository<FileStorage, String> {
+
+    List<FileStorage> findByClaimId(Long id);
+}
