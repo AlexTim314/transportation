50784e904abc475ae516393c21d3355f7364a471
user <user@user-0908>
2019-01-15 16:05:11 +0500

    Reorganized CommonController

--- src/main/java/org/ivc/transportation/controllers/CommonController.java
+++ src/main/java/org/ivc/transportation/controllers/CommonController.java
@@ -2,19 +2,10 @@ package org.ivc.transportation.controllers;
 
 import java.util.List;
 import org.ivc.transportation.entities.Department;
-import org.ivc.transportation.entities.Place;
 import org.ivc.transportation.entities.TransportDep;
-import org.ivc.transportation.services.DepartmentService;
-import org.ivc.transportation.services.PlaceService;
-import org.ivc.transportation.services.TransportDepService;
+import org.ivc.transportation.services.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,75 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommonController {
 
     @Autowired
-    private DepartmentService departmentService;
+    private CommonService commonService;
 
-    @Autowired
-    private TransportDepService transportDepService;
-
-    @Autowired
-    private PlaceService placeService;
-
-    @GetMapping("/management/departments")
-    public List<Department> getAllDepartments() {
-        return departmentService.getAllDepartments();
+    @GetMapping("/departments")
+    public List<Department> getDepartments() {
+        return commonService.findAllDepartments();
     }
 
-    @PostMapping("/management/department_create")
-    public Department createDepartment(@RequestBody Department department) {
-        return departmentService.saveDepartment(department);
-    }
-
-    @PutMapping("/management/department_update")
-    public Department updateDepartment(@RequestBody Department department) {
-        return departmentService.saveDepartment(department);
-    }
-
-    @DeleteMapping("/management/department_delete")
-    public ResponseEntity<String> deleteDepartment(@RequestBody Department department) {
-        departmentService.deleteDepartment(department);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping("/management/transportDeps")
-    public List<TransportDep> getAllTransportDeps() {
-        return transportDepService.getAllTransportDeps();
-    }
-
-    @PostMapping("/management/transportDep_create")
-    public TransportDep createTransportDep(@RequestBody TransportDep transportDep) {
-        return transportDepService.saveTransportDep(transportDep);
-    }
-
-    @PutMapping("/management/transportDep_update")
-    public TransportDep updateTransportDep(@RequestBody TransportDep transportDep) {
-        return transportDepService.saveTransportDep(transportDep);
-    }
-
-    @DeleteMapping("/management/transportDep_delete")
-    public ResponseEntity<String> deleteTransportDep(@RequestBody TransportDep transportDep) {
-        transportDepService.deleteTransportDep(transportDep);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping("/management/places")
-    public List<Place> getAllPlaces() {
-        return placeService.getAllPlaces();
-    }
-
-    @PostMapping("/management/place_create")
-    public Place createPlace(@RequestBody Place place) {
-        return placeService.savePlace(place);
-    }
-
-    @PutMapping("/management/place_update")
-    public Place updatePlace(@RequestBody Place place) {
-        return placeService.savePlace(place);
-    }
-
-    @DeleteMapping("/management/place_delete")
-    public ResponseEntity<String> deletePlace(@RequestBody Place place) {
-        placeService.deletePlace(place);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/transportDeps")
+    public List<TransportDep> getTransportDeps() {
+        return commonService.findAllTransportDeps();
     }
 
 }
--- src/main/java/org/ivc/transportation/services/CommonService.java
+++ src/main/java/org/ivc/transportation/services/CommonService.java
@@ -2,7 +2,9 @@ package org.ivc.transportation.services;
 
 import java.util.List;
 import org.ivc.transportation.entities.Department;
+import org.ivc.transportation.entities.TransportDep;
 import org.ivc.transportation.repositories.DepartmentRepository;
+import org.ivc.transportation.repositories.TransportDepRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,21 +15,20 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 @Transactional
-public class DepartmentService {
-    
+public class CommonService {
+
+    @Autowired
+    private TransportDepRepository transportDepRepository;
+
     @Autowired
     private DepartmentRepository departmentRepository;
     
-    public List<Department> getAllDepartments() {
+    public List<Department> findAllDepartments(){
         return departmentRepository.findAll();
     }
     
-    public Department saveDepartment(Department department) {
-        return departmentRepository.save(department);
-    }
-    
-    public void deleteDepartment(Department department) {
-        departmentRepository.delete(department);
+    public List<TransportDep> findAllTransportDeps(){
+        return transportDepRepository.findAll();
     }
     
 }
