bd4362df5243f2e994557a2d4074718a2ad4be2b
AlexTim <alextim@ivc.org>
2019-01-16 10:18:43 +0500

    Return VehicleModel.

--- src/main/java/org/ivc/transportation/entities/VehicleModel.java
+++ src/main/java/org/ivc/transportation/entities/VehicleModel.java
@@ -1,30 +1,46 @@
 package org.ivc.transportation.entities;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.lang.NonNull;
 
 /**
  *
- * @author alextim
+ * @author first
  */
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@EqualsAndHashCode(exclude = {})
-@ToString(exclude = {})
-@Entity
-class VehicleModel implements Serializable {
+@ToString(exclude = {"vehicleType"})
+@EqualsAndHashCode(exclude = {"vehicleType"})
+public class VehicleModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @NonNull
+    @Column(length = 1024)
+    private String ModelName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private VehicleType vehicleType;
+
+    public VehicleModel(String ModelName, VehicleType vehicleType) {
+        this.ModelName = ModelName;
+        this.vehicleType = vehicleType;
+    }
+
 }
