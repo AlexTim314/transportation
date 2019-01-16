3ed96edfd8807d51929e7fb12ae086bbf4425320
Soulyangor <User@WIN-QI6JQ19V8KI>
2019-01-16 10:11:00 +0500

    Added empty entities

---
+++ src/main/java/org/ivc/transportation/entities/Waypoint.java
@@ -0,0 +1,29 @@
+package org.ivc.transportation.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author Sokolov Slava
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {})
+@ToString(exclude = {})
+@Entity
+public class Waypoint implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
