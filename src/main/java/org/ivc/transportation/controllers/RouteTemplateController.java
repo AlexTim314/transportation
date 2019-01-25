629db322815d948e7d0a9aa9c9be412ca325d209
Soulyangor <User@WIN-QI6JQ19V8KI>
2019-01-25 11:26:03 +0500

    need for pull

---
+++ src/main/java/org/ivc/transportation/controllers/RouteTemplateController.java
@@ -0,0 +1,12 @@
+package org.ivc.transportation.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Sokolov Slava
+ */
+@RestController
+public class RouteTemplateController {
+
+}
---
+++ src/main/java/org/ivc/transportation/services/RouteTemplateService.java
@@ -0,0 +1,14 @@
+package org.ivc.transportation.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Sokolov Slava
+ */
+@Service
+@Transactional
+public class RouteTemplateService {
+
+}
