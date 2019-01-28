a1db2341463d3861bf8f4155290977ad00b218b9
user <user@debian-19012209>
2019-01-28 12:10:33 +0500

    added findrecordbyappointmentid

--- src/main/java/org/ivc/transportation/repositories/RecordRepository.java
+++ src/main/java/org/ivc/transportation/repositories/RecordRepository.java
@@ -16,4 +16,6 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
     @Query(value = "delete from record where claim_id = :claim_id", nativeQuery = true)
     void deleteByClaimId(@Param("claim_id") Long claimId);
 
+    @Query(value = "select record.* from record, appointment where record.id = appointment.record_id and appointment.id = :appointment_id", nativeQuery = true)
+    public Record findRecordByAppointmentId(@Param("appointment_id") Long appointmentId);
 }
