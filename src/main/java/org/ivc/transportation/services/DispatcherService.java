27cdd65fc16dca7f43c4b806534314ba9c0732d3
user <user@debian-19012209>
2019-01-30 14:36:29 +0500

    added appointmentController

--- src/main/java/org/ivc/transportation/services/DispatcherService.java
+++ src/main/java/org/ivc/transportation/services/DispatcherService.java
@@ -1,11 +1,20 @@
 package org.ivc.transportation.services;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 import org.ivc.transportation.entities.Appointment;
 import org.ivc.transportation.entities.Claim;
 import org.ivc.transportation.entities.Record;
+import org.ivc.transportation.entities.TransportDep;
+import org.ivc.transportation.repositories.AppointmentRepository;
 import org.ivc.transportation.repositories.ClaimRepository;
 import org.ivc.transportation.repositories.RecordRepository;
+import org.ivc.transportation.repositories.UserRepository;
+import org.ivc.transportation.utils.CompositeClaimRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,10 +27,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class DispatcherService {
 
     @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    @Autowired
     private RecordRepository recordRepository;
 
     @Autowired
     private ClaimRepository claimRepository;
+    
+    @Autowired
+    private UserRepository userRepository;
 
     public Claim findClaimByRecord(Record record) {
         return claimRepository.findByRecords(record);
@@ -30,5 +45,24 @@ public class DispatcherService {
     public Record findRecordByAppointment(Appointment appointment){
         return recordRepository.findRecordByAppointmentId(appointment.getId());
     }
+    
+    public List<CompositeClaimRecord> getAppointments(Principal principal) {
+        List<Appointment> appointmentList = appointmentRepository.findAppointmentsByTransportDep(findTransportDepByUser(principal).getId());
+        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
+        appointmentList.forEach(u -> result.add(
+                new CompositeClaimRecord(appointmentRepository.findClaimByAppointmentId(u.getId()),
+                        appointmentRepository.findRecordByAppointmentId(u.getId()), u)
+        ));
+        return result;
+    }
+    
+    public TransportDep findTransportDepByUser(Principal principal) {
+        if (principal != null) {
+            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+            return userRepository.findByUsername(loginedUser.getUsername()).getTransportDep();
+        }
+        return null;
+    }
+
 
 }
