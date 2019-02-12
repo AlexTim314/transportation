072c181a831f8e9b68788b445ac0237a9dbc1eb0
Lenovo-G580 <Lenovo-G580@192.168.100.5>
2019-02-12 15:40:55 +0500

    почти сделана страница управления

--- src/main/java/org/ivc/transportation/services/SuperManagerService.java
+++ src/main/java/org/ivc/transportation/services/SuperManagerService.java
@@ -14,6 +14,7 @@ import org.ivc.transportation.repositories.DepartmentRepository;
 import org.ivc.transportation.repositories.RecordRepository;
 import org.ivc.transportation.repositories.UserRepository;
 import org.ivc.transportation.utils.CompositeClaimRecord;
+import org.ivc.transportation.utils.CompositeDepartmentClaimRecords;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -47,32 +48,65 @@ public class SuperManagerService {
         return appointment == null ? new Appointment() : appointment;
     }
 
-    public List<CompositeClaimRecord> getCompositeClaimRecordsAll(Principal principal) {
-        List<Department> departments = departmentRepository.findDepartmentsBySuperManagerId(getUser(principal).getId());
-        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
-        if (!departments.isEmpty()) {
-            for (int i = 0; i < departments.size(); i++) {
-                recordRepository.findByDepartmentIdAndAffirmationDateIsNotNullAndActualIsTrue(departments.get(i).getId())
-                        .forEach(u -> result.add(new CompositeClaimRecord(new Claim(claimRepository.findByRecordId(u.getId())), u,
-                        prepareAppointment(appointmentRepository.getLastByRecordId(u.getId())))));
-            }
+    public List<CompositeDepartmentClaimRecords> getAffirmedClaimsAll(Principal principal) {
+        List<CompositeDepartmentClaimRecords> result = new ArrayList<CompositeDepartmentClaimRecords>();
+        if (!departmentRepository.findDepartmentsBySuperManagerWithAffirmedClaims(getUser(principal).getId()).isEmpty()) {
+            departmentRepository.findDepartmentsBySuperManagerWithAffirmedClaims(getUser(principal).getId()).forEach(u -> result.add(new CompositeDepartmentClaimRecords(u)));
+            result.forEach(u -> u.setCompositeClaimRecords(getCompositeClaimRecordsAll(u.getDepartment())));
         }
         return result;
     }
 
-    public List<CompositeClaimRecord> getCompositeClaimRecordsTimeFilter(Principal principal, ZonedDateTime dateStart, ZonedDateTime dateEnd) {
-        List<Department> departments = departmentRepository.findDepartmentsBySuperManagerId(getUser(principal).getId());
-        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
-        if (!departments.isEmpty()) {
-            for (int i = 0; i < departments.size(); i++) {
-                recordRepository.findByDepartmentIdAndAffirmationDateIsNotNullAndActualIsTrueTimeFilter(departments.get(i).getId(), dateStart, dateEnd)
-                        .forEach(u -> result.add(new CompositeClaimRecord(new Claim(claimRepository.findByRecordId(u.getId())), u,
-                        prepareAppointment(appointmentRepository.getLastByRecordId(u.getId())))));
-            }
+    public List<CompositeDepartmentClaimRecords> getAffirmedClaimsTimeFilter(ZonedDateTime dateStart, ZonedDateTime dateEnd, Principal principal) {
+        List<CompositeDepartmentClaimRecords> result = new ArrayList<CompositeDepartmentClaimRecords>();
+        if (!departmentRepository.findDepartmentsBySuperManagerWithAffirmedClaims(getUser(principal).getId()).isEmpty()) {
+            departmentRepository.findDepartmentsBySuperManagerWithAffirmedClaimsByTimeFilter(dateStart, dateEnd, getUser(principal).getId()).forEach(u -> result.add(new CompositeDepartmentClaimRecords(u)));
+            result.forEach(u -> u.setCompositeClaimRecords(getCompositeClaimRecordsTimeFilter(u.getDepartment(), dateStart, dateEnd)));
         }
         return result;
     }
 
+    private List<CompositeClaimRecord> getCompositeClaimRecordsAll(Department department) {
+        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
+        recordRepository.findByDepartmentIdAndAffirmationDateIsNotNullAndActualIsTrue(department.getId())
+                .forEach(u -> result.add(new CompositeClaimRecord(new Claim(claimRepository.findByRecordId(u.getId())), u,
+                prepareAppointment(appointmentRepository.getLastByRecordId(u.getId())))));
+        return result;
+    }
+
+    private List<CompositeClaimRecord> getCompositeClaimRecordsTimeFilter(Department department, ZonedDateTime dateStart, ZonedDateTime dateEnd) {
+        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
+        recordRepository.findByDepartmentIdAndAffirmationDateIsNotNullAndActualIsTrueTimeFilter(department.getId(), dateStart, dateEnd)
+                .forEach(u -> result.add(new CompositeClaimRecord(new Claim(claimRepository.findByRecordId(u.getId())), u,
+                prepareAppointment(appointmentRepository.getLastByRecordId(u.getId())))));
+        return result;
+    }
+
+//    public List<CompositeClaimRecord> getCompositeClaimRecordsAll(Principal principal) {
+//        List<Department> departments = departmentRepository.findDepartmentsBySuperManagerId(getUser(principal).getId());
+//        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
+//        if (!departments.isEmpty()) {
+//            for (int i = 0; i < departments.size(); i++) {
+//                recordRepository.findByDepartmentIdAndAffirmationDateIsNotNullAndActualIsTrue(departments.get(i).getId())
+//                        .forEach(u -> result.add(new CompositeClaimRecord(new Claim(claimRepository.findByRecordId(u.getId())), u,
+//                        prepareAppointment(appointmentRepository.getLastByRecordId(u.getId())))));
+//            }
+//        }
+//        return result;
+//    }
+//
+//    public List<CompositeClaimRecord> getCompositeClaimRecordsTimeFilter(Principal principal, ZonedDateTime dateStart, ZonedDateTime dateEnd) {
+//        List<Department> departments = departmentRepository.findDepartmentsBySuperManagerId(getUser(principal).getId());
+//        List<CompositeClaimRecord> result = new ArrayList<CompositeClaimRecord>();
+//        if (!departments.isEmpty()) {
+//            for (int i = 0; i < departments.size(); i++) {
+//                recordRepository.findByDepartmentIdAndAffirmationDateIsNotNullAndActualIsTrueTimeFilter(departments.get(i).getId(), dateStart, dateEnd)
+//                        .forEach(u -> result.add(new CompositeClaimRecord(new Claim(claimRepository.findByRecordId(u.getId())), u,
+//                        prepareAppointment(appointmentRepository.getLastByRecordId(u.getId())))));
+//            }
+//        }
+//        return result;
+//    }
     private AppUser getUser(Principal principal) {
         if (principal != null) {
             User loginedUser = (User) ((Authentication) principal).getPrincipal();
