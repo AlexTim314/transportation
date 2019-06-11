a0e18eddfbc1b570aafee8f06a6fc0f73a567d5b
user <user@debian-19012209>
2019-06-11 16:37:01 +0500

    added transportDep OneToOne place

---
+++ src/main/java/org/ivc/transportation/entities/RefuelingStation.java
@@ -0,0 +1,41 @@
+package org.ivc.transportation.entities;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author Sokolov Slava
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"place"})
+@ToString(exclude = {"place"})
+@Entity
+@Table(name = "refueling_station")
+public class RefuelingStation implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne
+    private Place place;
+
+}
