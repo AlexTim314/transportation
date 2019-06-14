95e2935d44a360ec1199b4f48496875352dabff8
user <user@debian-19012209>
2019-06-14 16:43:53 +0500

    tdDriverInfo

--- src/main/java/org/ivc/transportation/controllers/AppointmentController.java
+++ src/main/java/org/ivc/transportation/controllers/AppointmentController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.ivc.transportation.utils.tdDriverInfo;
 
 /**
  *
@@ -42,6 +43,11 @@ public class AppointmentController {
     @GetMapping("/dispatcher/appointments")
     public List<CompositeClaimRecord> getAppointments(Principal principal) {
         return dispatcherService.getAppointments(principal);
+    }
+
+    @GetMapping("/dispatcher/driverInfo")
+    public List<tdDriverInfo> getDriverInfo(Principal principal) {
+        return dispatcherService.getTdDriverInfo(principal);
     }
 
     @GetMapping("/dispatcher/appointments1")
